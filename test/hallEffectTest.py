f137544ff031fd6adf2e8b8e87224e3b44991d6f
James Garrett <jaagarre@ucsc.edu>
2021-07-22 11:52:29 -0700

    Add test for hall effect sensor

---
+++ test/hallEffectTest.py
@@ -0,0 +1,16 @@
+﻿import RPI.GPO as GPIO
+import time
+
+hallPin = 4
+
+GPIO.setmode(GPIO_BOARD)
+GPIO.setup(hallPin, GPIO.IN)
+GPIO.setwarnings(False)
+
+try:
+    while True:
+        if(GPIO.input(hallpin) == False):
+            print("Magnet Detected!\n")
+            time.sleep(0.5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
