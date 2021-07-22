2eca3cd10a2328c17123113747bb039369170135
jamesagarrett <jaagarre@ucsc.edu>
2021-07-22 21:31:46 +0000

    Bug fixes and slight changes to hall test

--- test/hallEffectTest.py
+++ test/hallEffectTest.py
@@ -1,16 +1,19 @@
-﻿import RPI.GPO as GPIO
+﻿import RPi.GPIO as GPIO
 import time
 
-hallPin = 4
+hallPin = 7
 
-GPIO.setmode(GPIO_BOARD)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(hallPin, GPIO.IN)
 GPIO.setwarnings(False)
 
 try:
     while True:
-        if(GPIO.input(hallpin) == False):
+        if(GPIO.input(hallPin) == False):
             print("Magnet Detected!\n")
-            time.sleep(0.5)
+            time.sleep(0.25)
+        else:
+            print("No Magnet...\n")
+            time.sleep(0.25)
 except KeyboardInterrupt:
     GPIO.cleanup()
