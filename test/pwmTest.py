da84eadd37a9b892a5169ed0d366131653ccb6ff
James Garrett <jaagarre@ucsc.edu>
2021-07-24 17:00:53 -0700

    Compare desired versus actual RPM values

--- test/pwmTest.py
+++ test/pwmTest.py
@@ -5,20 +5,28 @@
 from __future__ import division
 import time
 from math import sin, cos, radians
-
-# Import the PCA9685 module.
 import PCA9685
+import RPi.GPIO as GPIO
+
+pwm = PCA9685.PCA9685()
+pwm.set_pwm_freq(160)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN)
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(10, GPIO.IN)
+GPIO.setwarnings(False)
 
 def calculatePWM(wheel, rpm, spinCCW):
-    #####################################
-    ##
-    ##  VARIABLE DECLARATION
-    ##
-    #####################################
-
     val = 0     ##The corresponding PWM value for the given wheel RPM.
-    
-    #####################################
+  
+    CCW_COEFS = [[-0.0002, 1.3091, 1051.4177],
+                 [-0.0001, 1.2512, 1028.5241],
+                 [-0.0000, 1.3511, 1027.8794]]
+
+    CW_COEFS = [[0.0003, -1.3877, 958.0234],
+                [0.0000, -1.2198, 968.0393],
+                [0.0001, -1.3325, 963.6987]]
 
     if(spinCCW):
         val = CCW_COEFS[wheel][0] * rpm**2 \
@@ -31,17 +39,34 @@ def calculatePWM(wheel, rpm, spinCCW):
     
     return round(val)
 
-# Initialize the PCA9685 using the default address (0x40).
-pwm = PCA9685.PCA9685()
-pwm.set_pwm_freq(160)
+def getRPM(pin):
+    revolvs = 10
+    average = 0
+
+    for _ in range (revolvs):
+        while(GPIO.input(pin) == True):
+            continue
+
+        while(GPIO.input(pin) == False):
+            start = time.time()
+        while(GPIO.input(pin) == True):
+            end = time.time()
+
+        revolution = (1/(end-start))*60
+        average += revolution
+
+    average /= revolvs
+    return average
 
 maxSpeed = 200
 wheelSpeeds = [0]*3             
 speedVars = [0]*2
 rotationCoeff = 0
 wheelPWMs = [0]*3
-
+wantRPMs = [0]*3
+realRPMs = [0]*3
 PWM_PORTS = [7, 5, 6]
+pins = [21, 4, 10]
 
 WHEEL_LOCATIONS = [210, 330, 90]
 
@@ -75,20 +100,32 @@ wheelSpeeds[2] = (-wheelSpeeds[1] - wheelSpeeds[0] + rotationCoeff)
 for x in range (3):
     if(wheelSpeeds[x] == 0):
         wheelPWMs[x] = 1000
+        wantRPMs[x] = 0
     elif(wheelSpeeds[x] > 0):
-        wheelPWMs[x] = calculatePWM(x, wheelSpeeds[x]*maxSpeed, True)
+        wantRPMs[x] = wheelSpeeds[x]*maxSpeed
+        wheelPWMs[x] = calculatePWM(x, wantRPMs[x], True)
     else:
-        wheelPWMs[x] = calculatePWM(x, abs(wheelSpeeds[x]*maxSpeed), False)
- 
+        wantRPMs[x] = abs(wheelSpeeds[x]*maxSpeed)
+        wheelPWMs[x] = calculatePWM(x, wantRPMs[x], False)
+
+pwm.set_pwm(PWM_PORTS[0], 0, wheelPWMs[0])
+pwm.set_pwm(PWM_PORTS[1], 0, wheelPWMs[1])
+pwm.set_pwm(PWM_PORTS[2], 0, wheelPWMs[2])
+time.sleep(0.25)
+
+for x in range (3):
+    if (wantRPMs[x] < 5):
+        realRPMs[x] = 0
+    else:
+        realRPMs[x] = getRPMs[pins[x]]
+
 print("Speeds: ", wheelSpeeds)
-print("RPMs: ", abs(wheelSpeeds[0]*maxSpeed), abs(wheelSpeeds[1]*maxSpeed), abs(wheelSpeeds[2]*maxSpeed))
 print("PWMs: ", wheelPWMs)
+print("RPMs (Wanted/Actual): %.2f/%.2f, %.2f/%.2f, %.2f/%.2f" % (wantRPMs[0], realRPMs[0], wantRPMs[1], realRPMs[1], wantRPMs[2], realRPMs[2]))
 
 try:
 	while(True):
-		pwm.set_pwm(PWM_PORTS[0], 0, wheelPWMs[0])
-		pwm.set_pwm(PWM_PORTS[1], 0, wheelPWMs[1])
-		pwm.set_pwm(PWM_PORTS[2], 0, wheelPWMs[2])
+		continue
 except KeyboardInterrupt:
     pwm.set_pwm(PWM_PORTS[0], 0, 1000)
     pwm.set_pwm(PWM_PORTS[1], 0, 1000)
